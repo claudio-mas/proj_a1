e09509f8083e72c1bbdf00ab5904ed53811823d5
Claudio Mas <claudio.mas@gmail.com>
2024-11-29 16:04:48 -0300

    config media

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from apps.estoque.views import index
 from apps.clientes.views import clientes
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('clientes/', clientes, name='clientes'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
