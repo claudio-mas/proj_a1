6cdd36add6258fd0055046ba5c4c6a27bc047906
Claudio Mas <claudio.mas@gmail.com>
2024-11-30 13:27:51 -0300

    gestão de marcas

--- apps/estoque/admin.py
+++ apps/estoque/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Marca
+from .models import Marca, Grupo, Produto
 
 
 # Register your models here.
@@ -10,4 +10,18 @@ class MarcaAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = ('grupo', 'idmarca')
+    # list_editable = ('grupo')
+    # search_fields = ('grupo')
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('produto', 'idgrupo', 'preco', 'imagem')
+    list_editable = ('preco', 'imagem')
+    search_fields = ('produto', 'marca')
+
+
 admin.site.register(Marca, MarcaAdmin)
+admin.site.register(Grupo, GrupoAdmin)
+admin.site.register(Produto, ProdutoAdmin)
--- apps/estoque/models.py
+++ apps/estoque/models.py
@@ -23,3 +23,32 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.marca
+
+
+class Grupo(models.Model):
+    id = models.AutoField(primary_key=True)
+    idmarca = models.ForeignKey(Marca, on_delete=models.CASCADE, verbose_name="Marca")
+    grupo = models.CharField(max_length=50, default="", verbose_name="Grupo")
+
+    class Meta:
+        verbose_name_plural = "2.2- Grupos"
+        verbose_name = "Grupo"
+
+    def __str__(self):
+        return self.grupo
+
+
+class Produto(models.Model):
+    id = models.AutoField(primary_key=True)
+    idgrupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, verbose_name="Grupo")
+    produto = models.CharField(max_length=50, default="", verbose_name="Produto")
+    descricao = models.TextField(null=True, blank=True, verbose_name="Descrição")
+    imagem = models.ImageField(upload_to='produtos/', null=True, blank=True, verbose_name="Imagem")
+    preco = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
+
+    class Meta:
+        verbose_name_plural = "2.3- Produtos"
+        verbose_name = "Produto"
+
+    def __str__(self):
+        return self.produto
