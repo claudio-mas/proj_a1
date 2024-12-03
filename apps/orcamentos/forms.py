2583455420d3ecf911d9228bf80b41ec8dd43ba4
Claudio Mas <claudio.mas@gmail.com>
2024-12-03 17:11:59 -0300

    itens do orçamento

--- apps/orcamentos/forms.py
+++ apps/orcamentos/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Orcamento
+from .models import Orcamento, OrcamentoItem
 
 
 class OrcamentoForm(forms.ModelForm):
@@ -9,4 +9,18 @@ class OrcamentoForm(forms.ModelForm):
         widgets = {
             'data': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
             'validade': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
+        }
+
+
+class OrcamentoItemForm(forms.ModelForm):
+    class Meta:
+        model = OrcamentoItem
+        fields = ['id', 'idproduto', 'idproduto2', 'quantidade', 'valor', 'desconto']
+
+        widgets = {
+            'idproduto': forms.Select(attrs={'class': 'form-control'}),
+            'idproduto2': forms.Select(attrs={'class': 'form-control'}),
+            'quantidade': forms.NumberInput(attrs={'class': 'form-control'}),
+            'valor': forms.NumberInput(attrs={'class': 'form-control'}),
+            'desconto': forms.NumberInput(attrs={'class': 'form-control'}),
         }
